32ac0b15b94b7ab187690fa1adea594b1892aa8d
huuthangkhuat <huuthang.khuat21@gmail.com>
2025-09-18 16:24:17 +1000

    Update bug fixes and improve error handling in app.js

--- app.js
+++ app.js
@@ -87,8 +87,7 @@ async function get_current_network(){
 async function connectWallet() {
     if (window.ethereum) {
         try {
-            const accounts = await get_current_eth_address();
-            userAccount = accounts[0];
+            userAccount = await get_current_eth_address();
             console.log("Connected account:", userAccount);
             web3 = new Web3(window.ethereum);
             votingContract = new web3.eth.Contract(contractABI, contractAddress);
